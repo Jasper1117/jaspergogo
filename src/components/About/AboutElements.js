0f5dde32cce2251785684924d16a3ef7619ac921
Jasper <jasperxu1117@gmail.com>
2022-03-04 22:00:43 +0800

    adjuest about page

--- src/components/About/AboutElements.js
+++ src/components/About/AboutElements.js
@@ -1,30 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
 import self from "../../img/self-pic.svg"
-import aboutBg from "../../img/aboutBg.svg"
+import {NavLink} from "react-router-dom"
 
 const AboutElements = () => {
+  // const texts = ["JASPER", "XUHE"];
+  // let count=0;
+  // let index=0;
+  // let currentText ="";
+  // let letter ="";
+
+  // (function type(){
+  //   if(count === texts.length){
+  //     count=0;
+  //   }
+  //   currentText = texts[count];
+  //   letter = currentText.slice(0, ++index);
+
+  //   document.querySelector('.typing').textContent = letter;
+  //   if(letter.length === currentText.length){
+  //     count++;
+  //     index=0;
+  //   }
+  //   setTimeout(type, 200);
+  // }());
+
+
   return <AboutElementStyle>
       <div className="about-con">
           <div className="about-left">
             <img src={self} alt="" />
-            <div className="about-left-text">
-              <p>許根權<br/>Xu Gen Quan, Jasper</p>
-     
-            </div>
-            
           </div>
           <div className="about-right">
-            <h2>About Me</h2>
+          <h2>Hiello! I'm</h2>
+          <h1 className="typing">Jasper Xu</h1>
+         
             <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum nihil nisi voluptatem asperiores tempore necessitatibus possimus iusto. Nesciunt, deserunt praesentium.</p>
+            <NavLink to="/contact" className="contact" > Contact
+  
+             </NavLink>
           </div>
-       
+          
       </div>
       
   </AboutElementStyle>;
 };
 
 const AboutElementStyle = styled.div`
+    background-color: #3F3D56;
+    color:white ;
+
   .about-con{
     display: flex;
     align-items: center; 
@@ -33,69 +58,91 @@ const AboutElementStyle = styled.div`
 
     .about-left{
       width: 50%;
-      padding-left: 30px;
+      padding-left: 5rem;
       display: flex;
-      .about-left-text{
-        display: grid;
-        grid-template-columns: repeat(1 2fr);
-        padding: 0rem 2rem;
-        margin: auto;
-        font-weight: 300;
-      }
+      
       img{
         width: 60%;
       }
     }
     .about-right{
       width: 50%;
-      padding-left: 6rem;
-      padding-right: 30px;
       display: flex;
-      justify-content: center;
-      align-items: center;
+      justify-content: flex-start;
       flex-direction: column;
+      padding-right: 5rem;
     }
-    .aboutBg{
-      
+    h1{
+      font-size: 6rem;
+      position: relative;
+      width:max-content ;
+    }
+
+    h1::before,
+    h1::after{
+      content: "";
       position: absolute;
-      z-index: -3;
-
-      width: 100%;
+      top:0 ;
+      left:0 ;
+      right:0 ;
+      bottom: 0;
     }
+
+    h1::before{
+      background-color: #3F3D56;
+      animation:typewriter 1.1s steps(9) forwards ;
+      left: 0;
+    }
+    h1::after{
+      width: 0.05em;
+      background: white;
+      
+      animation:typewriter 1.1s steps(11) forwards ,
+      brink 1.2s infinite ease;
+    }
+    @keyframes brink {
+      0%{
+        opacity:0% ;
+      }
+      100%{
+        opacity:100% ;
+      }
+    }
+
+    @keyframes typewriter {
+      to{
+        left: 100%;
+      }
+    }
+
+    h2{
+      font-size: 2rem;
+    }
+    p{
+      font-size: 1.3rem;
+      padding-top: 1.5rem;
+    }
+    .contact{
+      display:flex ;
+      justify-content:center ;
+      align-items:center ;
+      font-size: 1.5rem;
+      width: 120px;
+      height: 5vh;
+      margin: 2rem 0rem;
+       border-radius: 50px;
+       background-color: #7aa1db;
+       padding: 0.3rem 1.2rem;
+       color: white;
+       text-decoration: none;
+       transition: all 0.2s ease-in-out;
+       :hover{
+           box-shadow: 0px 0px 10px 1px white;;
+           color: #364047;
+           background-color: #acc9f3;
+           font-size: 1.6rem;
+       }
   }
-`; 
-
+}
+`;
 export default AboutElements;
-
-
-// .about-con{
-//   display: grid;
-//   grid-template-columns: repeat(2, 1fr);
-//   padding:  7rem 5rem ;
-//   position: absolute;
-//   align-items: center;
-//   .about-left{
-//     display: flex;
-//     padding-left: 4rem;
-//     img{
-//       width: 60%;
-//     }
-//     .about-left-text{
-//       padding-top: 14rem;
-//     }
-//   }
-
-//   .about-right{
-//     display: flex;
-//     flex-direction: column;
-//     align-items: center;
-//   }
-// }
-
-// .aboutBg{
-//   width: 100%;
-//   position: relative;
-//   z-index: -1;
-//   top:84%;
-  
-// }
